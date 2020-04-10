c18d7a3bcf0c2943008f4eb6eedd97f5def0c760
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-10 10:40:13 -0400

    Tornando membros privados

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 main() => runApp(PerguntaApp());
 
-class PerguntaAppState extends State<PerguntaApp>{
-  var perguntaSelecionada = 0;
+class _PerguntaAppState extends State<PerguntaApp>{
+  var _perguntaSelecionada = 0;
 
-  void responder() {
+  void _responder() {
     setState(() {
-    perguntaSelecionada++;
+    _perguntaSelecionada++;
       });
-    print(perguntaSelecionada);
+    print(_perguntaSelecionada);
   }
   @override
   Widget build(BuildContext context){
@@ -23,18 +23,18 @@ class PerguntaAppState extends State<PerguntaApp>{
         ),
         body: Column(
           children: <Widget>[
-            Text(perguntas[perguntaSelecionada]),
+            Text(perguntas[_perguntaSelecionada]),
             RaisedButton(
               child: Text('Resposta 1'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             RaisedButton(
               child: Text('Resposta 2'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
             RaisedButton(
               child: Text('Resposta 3'),
-              onPressed: responder,
+              onPressed: _responder,
             ),
           ],
         ),  
@@ -45,7 +45,7 @@ class PerguntaAppState extends State<PerguntaApp>{
 
 class PerguntaApp extends StatefulWidget{
   
-  PerguntaAppState createState(){
-    return PerguntaAppState();
+  _PerguntaAppState createState(){
+    return _PerguntaAppState();
   }
 } 
