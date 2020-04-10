412add9a3535bfc8c5654483d7496f63f6b1166e
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-10 11:19:12 -0400

    Usando tipo Map nas perguntas

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -14,9 +14,20 @@ class _PerguntaAppState extends State<PerguntaApp>{
   }
   @override
   Widget build(BuildContext context){
+    // final List<Map<String, Object>> perguntas =
     final perguntas = [
-      'Qual é sua cor favorita?',
-      'Qual é o seu animal favorito?',
+      {
+      'texto':'Qual é sua cor favorita?',
+      'respostas':['Preto', 'Vermelho', 'Verde', 'Branco'],
+      },
+      {
+      'texto':'Qual é o seu animal favorito?',
+      'respostas':['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      },
+      {
+      'texto':'Qual é o seu instrutor favorito?',
+      'respostas':['Maria', 'João', 'Leo', 'Pedro'],
+      },
     ];
     return MaterialApp(
       home: Scaffold(
@@ -25,7 +36,7 @@ class _PerguntaAppState extends State<PerguntaApp>{
         ),
         body: Column(
           children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
+            Questao(perguntas[_perguntaSelecionada]['texto']),
             Resposta('Resposta 1', _responder),
             Resposta('Resposta 2', _responder),
             Resposta('Resposta 3', _responder),
