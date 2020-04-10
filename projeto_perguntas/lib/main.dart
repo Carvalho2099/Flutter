caa93f3bf5d8d2f5da5d6bd67747b26506dfd589
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-10 12:16:53 -0400

    Exibindo resultado

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -4,39 +4,41 @@ import './resposta.dart';
 
 main() => runApp(PerguntaApp());
 
-class _PerguntaAppState extends State<PerguntaApp>{
+class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
-     // final List<Map<String, Object>> perguntas =
-    final _perguntas = const[
-      {
-      'texto':'Qual é sua cor favorita?',
-      'respostas':['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-      'texto':'Qual é o seu animal favorito?',
-      'respostas':['Coelho', 'Cobra', 'Elefante', 'Leão'],
-      },
-      {
-      'texto':'Qual é o seu instrutor favorito?',
-      'respostas':['Maria', 'João', 'Leo', 'Pedro'],
-      },
-    ];
+  // final List<Map<String, Object>> perguntas =
+  final _perguntas = const [
+    {
+      'texto': 'Qual é sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    },
+  ];
 
   void _responder() {
-    if(temPerguntaSelecionada){
-    setState(() {
-    _perguntaSelecionada++;
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
       });
     }
   }
 
-  bool get temPerguntaSelecionada{
+  bool get temPerguntaSelecionada {
     return _perguntaSelecionada < _perguntas.length;
   }
+
   @override
-  Widget build(BuildContext context){
- 
-    List<String> respostas = temPerguntaSelecionada ? _perguntas[_perguntaSelecionada]['respostas']: null;
+  Widget build(BuildContext context) {
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada]['respostas']
+        : null;
     // List<Widget> widgets = respostas.map((t) => Resposta(t, _responder)).toList();
     // for(var textoResp in respostas){
     //   widgets.add(Resposta(textoResp, _responder));
@@ -47,20 +49,26 @@ class _PerguntaAppState extends State<PerguntaApp>{
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: temPerguntaSelecionada ? Column(
-          children: <Widget>[
-            Questao(_perguntas[_perguntaSelecionada]['texto']),
-            ...respostas.map((t) => Resposta(t, _responder)).toList(),
-          ],
-        ): null,  
+        body: temPerguntaSelecionada
+            ? Column(
+                children: <Widget>[
+                  Questao(_perguntas[_perguntaSelecionada]['texto']),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Parabéns!',
+                  style: TextStyle(fontSize: 28),
+                ),
+              ),
       ),
     );
   }
 }
 
-class PerguntaApp extends StatefulWidget{
-  
-  _PerguntaAppState createState(){
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
     return _PerguntaAppState();
   }
-} 
+}
