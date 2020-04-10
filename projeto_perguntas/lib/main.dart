552cb758690e6d3753fef49a339d08906479c5a5
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-10 11:10:01 -0400

    Alterando estilo do botão passando função callback

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -11,7 +11,6 @@ class _PerguntaAppState extends State<PerguntaApp>{
     setState(() {
     _perguntaSelecionada++;
       });
-    print(_perguntaSelecionada);
   }
   @override
   Widget build(BuildContext context){
@@ -27,9 +26,9 @@ class _PerguntaAppState extends State<PerguntaApp>{
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1'),
-            Resposta('Resposta 2'),
-            Resposta('Resposta 3'),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder),
           ],
         ),  
       ),
--- projeto_perguntas/lib/resposta.dart
+++ projeto_perguntas/lib/resposta.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String texto;
+  final void Function() quandoSelecionado;
 
-  Resposta(this.texto);
+  Resposta(this.texto, this.quandoSelecionado);
   @override
   Widget build(BuildContext context) {
-    return RaisedButton(
-      child: Text(texto),
-      onPressed: null,
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.blue,
+        child: Text(texto),
+        onPressed: quandoSelecionado,
+      ),
     );
   }
 }
