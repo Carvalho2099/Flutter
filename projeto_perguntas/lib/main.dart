41d55efd92e6b1486f606f914ec12769773e8126
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-10 13:38:42 -0400

    Widget resultado

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
 import './resposta.dart';
+import './resultado.dart';
 
 main() => runApp(PerguntaApp());
 
@@ -56,12 +57,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   ...respostas.map((t) => Resposta(t, _responder)).toList(),
                 ],
               )
-            : Center(
-                child: Text(
-                  'Parabéns!',
-                  style: TextStyle(fontSize: 28),
-                ),
-              ),
+            : Resultado(),
       ),
     );
   }
