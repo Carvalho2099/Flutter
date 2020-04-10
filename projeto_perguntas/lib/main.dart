1fa98aa354ee87db8d796a7932640d426117a287
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-10 11:49:11 -0400

    Lista para Widgets

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -29,6 +29,12 @@ class _PerguntaAppState extends State<PerguntaApp>{
       'respostas':['Maria', 'João', 'Leo', 'Pedro'],
       },
     ];
+    List<String> respostas = perguntas[_perguntaSelecionada]['respostas'];
+    // List<Widget> widgets = respostas.map((t) => Resposta(t, _responder)).toList();
+    // for(var textoResp in respostas){
+    //   widgets.add(Resposta(textoResp, _responder));
+    // }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -37,9 +43,7 @@ class _PerguntaAppState extends State<PerguntaApp>{
         body: Column(
           children: <Widget>[
             Questao(perguntas[_perguntaSelecionada]['texto']),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            ...respostas.map((t) => Resposta(t, _responder)).toList(),
           ],
         ),  
       ),
