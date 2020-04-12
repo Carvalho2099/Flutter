65e54848fca0179d4917632049457a9c6c4bf2ff
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-12 18:06:13 -0400

    dividindo App em Widgets

---
+++ projeto_despesas/lib/components/transaction_user.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './transactions_form.dart';
+import 'transactions_list.dart';
+import '../models/transaction.dart';
+
+class TransactionUser extends StatefulWidget {
+  @override
+  _TransactionUserState createState() => _TransactionUserState();
+}
+
+class _TransactionUserState extends State<TransactionUser> {
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TransactionList(_transactions),
+        TransactionForm(),
+      ],
+    );
+  }
+}
