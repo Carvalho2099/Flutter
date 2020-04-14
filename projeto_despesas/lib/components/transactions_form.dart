08d83a8acad55c26352448bef154413609d48ee0
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-13 20:07:07 -0400

    Correção formulário

--- projeto_despesas/lib/components/transactions_form.dart
+++ projeto_despesas/lib/components/transactions_form.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
+class TransactionForm extends StatefulWidget {
   final void Function(String, double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  _TransactionFormState createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
 
   _subimitForm(){
     final title = titleController.text;
@@ -14,7 +20,7 @@ class TransactionForm extends StatelessWidget {
     if(title.isEmpty || value <= 0){
       return;
     }
-    onSubmit(title, value);
+    widget.onSubmit(title, value);
   }
 
   @override
