6f776f7800bf2b5c9499c398d43bc12705c1bae7
Felipe Carvalho <31414765+Carvalhotkd@users.noreply.github.com>
2020-04-13 20:18:43 -0400

    Fechando Formulário(modal)

--- projeto_despesas/lib/main.dart
+++ projeto_despesas/lib/main.dart
@@ -48,6 +48,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
